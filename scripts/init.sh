023942c24934ae0ddc9e1da4cf1c84c3f1c33876
yamenzk <98694759+yamenzk@users.noreply.github.com>
2025-03-25 15:57:21 +0400

    DB Root

--- scripts/init.sh
+++ scripts/init.sh
@@ -37,7 +37,9 @@ sed -i '/redis/d' ./Procfile
 bench new-site dev.localhost \
 --mariadb-root-password 123 \
 --admin-password admin \
+--db-root-username root \
 --no-mariadb-socket
+
 
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
